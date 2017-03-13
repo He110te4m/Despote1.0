68624363bf5a5bed52fdee0b4ba1c2e0a3d15555
He110te4m <He110te4m@gmail.com>
2017-03-13 22:07:28 +0800

    update

---
+++ view/login.php
@@ -0,0 +1,27 @@
+<div style="position: fixed; top: 50%; left: 50%; width:50%; height: 50%; -webkit-transform: translateX(-50%) translateY(-50%); -moz-transform: translateX(-50%) translateY(-50%); -ms-transform: translateX(-50%) translateY(-50%); transform: translateX(-50%) translateY(-50%);">
+    <div class="col-sm-8 col-sm-offset-2">
+        <form class="form-horizontal">
+            <div class="form-group">
+                <label class="control-label col-md-2" for="username">用户名</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="username" placeholder="用户名">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-md-2" for="password">密码</label>
+                <div class="col-md-8">
+                    <input type="password" class="form-control" id="password" placeholder="密码">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-4 col-sm-offset-4">
+                    <label class="checkbox-inline"><input type="checkbox">保持登录状态</label>
+                </div>
+            </div>
+            <div class="btn-group col-sm-offset-3" role="group">
+                <button type="submit" class="btn btn-primary btn-lg">登录</button>
+                <button type="submit" class="btn btn-primary btn-lg">忘记密码</button>
+            </div>
+        </form>
+    </div>
+</div>
