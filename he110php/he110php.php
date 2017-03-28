b6a9f14c69d8a7100bbae34b4fb1706015593099
He110te4m <He110te4m@gmail.com>
2017-03-28 15:30:19 +0800

    更新常量定义

--- he110php/he110php.php
+++ he110php/he110php.php
@@ -5,11 +5,18 @@
  */
 
 // 初始化常量
+// 框架目录
 defined('FRAME_PATH') or define('FRAME_PATH', __DIR__ . '/');
+// 应用目录
 defined('APP_PATH') or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/');
+// 是否为 Debug 模式
 defined('APP_DEBUG') or define('APP_DEBUG', false);
+// 配置目录
 defined('CONFIG_PATH') or define('CONFIG_PATH', APP_PATH . 'config/');
+// 运行目录
 defined('RUNTIME_PATH') or define('RUNTIME_PATH', APP_PATH . 'runtime/');
+// 第三方库
+defined('LIB_PATH') or define('LIB_PATH', APP_URL . 'application/lib/');
 
 // 包含配置文件
 require APP_PATH . 'config/config.php';
